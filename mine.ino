d8e4ef2a037582d4bce426c6a1de1f609119c57d
Mateusz Zając <deejay.zajac@gmail.com>
2020-03-29 20:08:41 +0200

    Added status cmd and side handling

--- mine.ino
+++ mine.ino
@@ -1,11 +1,15 @@
 // BLE Board controls
 const int ledBLE = 13;
 
+enum Side {Right, Left, None};
+
 // Auxillary equipment setup
 const int stepPin = 3;
 const int dirPin = 4;
 const int buttonPin = 2;
+const int enablePin = 5;
 int buttonState, timePressed, startTime = 0;
+enum Side side = None;
 
 void setup() {
   Serial.begin(115200);  //initial the Serial
@@ -13,29 +17,30 @@ void setup() {
   pinMode(ledBLE, OUTPUT);
   pinMode(stepPin,OUTPUT);
   pinMode(dirPin,OUTPUT);
+  pinMode(enablePin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
+    side = Left;
     if(startTime == 0) {
       startTime = millis();  
     } else {
       timePressed = millis() - startTime;
     }
   } else {
+    side = None;
     startTime = 0;
     timePressed = 0;
-  }
-  if(timePressed > 1000 && timePressed % 1000 == 0) {
-    Serial.println(timePressed / 1000);
   }
 
   if (Serial.available())  {
     String command = Serial.readStringUntil('\n');
     if (command.indexOf("rotate") == 0) {
-      Serial.println("Rotated");//send what has been received
+      digitalWrite(enablePin,HIGH);
+      delayMicroseconds(500);
       digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
      
       // Makes 200 pulses for making one full cycle rotation
@@ -45,11 +50,20 @@ void loop() {
         digitalWrite(stepPin,LOW);
         delayMicroseconds(500);
       }
+      Serial.println("Rotated");//send what has been received
+      digitalWrite(enablePin,LOW);
+    } else if (command.indexOf("status") == 0) {
+      if(timePressed > 1000) {
+        int floorTime = (timePressed - timePressed % 1000) / 1000;
+        if(side == Left) {
+          Serial.println("L:" + floorTime);
+        } else {
+          Serial.println("R:" + floorTime);
+        }
+      }
     } else {
       Serial.println("Command not recognized");
     }
-    Serial.write(command.c_str());//send what has been received
-    Serial.println();   //print line feed character
   }
 }
 
